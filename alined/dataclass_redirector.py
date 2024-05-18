5725f86a72640af74e60b8dcb5ee4c897c01deae
AWeirdScratcher <aweirdscratcher@gmail.com>
2024-05-18 12:38:29 +0800

    added audio, file, location, sticker redirectors

--- alined/dataclass_redirector.py
+++ alined/dataclass_redirector.py
@@ -3,9 +3,14 @@ from .dataclass import (
     SourceGroupChatForMessageEvents,
     SourceUser,
     SourceMultiPersonChatForMessageEvents,
+    WebhookAudioContentProviderExternal,
+    WebhookAudioMessage,
+    WebhookFileMessage,
+    WebhookLocationMessage,
     WebhookMediaContentProviderExternal,
     WebhookMediaContentProviderLINE,
     WebhookImageMessage,
+    WebhookStickerMessage,
     WebhookTextMessage,
     WebhookVideoMessage,
 )
@@ -25,6 +30,7 @@ def redirect_dataclass(d: dict) -> MessageEvent:
 
         if msg_t == "text":
             kwargs.update({"message": WebhookTextMessage(**d["message"])})
+
         elif msg_t in {"image", "video"}:
             k = {**d["message"]}
 
@@ -44,6 +50,31 @@ def redirect_dataclass(d: dict) -> MessageEvent:
                 kwargs.update({"message": WebhookImageMessage(**k)})
             else:
                 kwargs.update({"message": WebhookVideoMessage(**k)})
+
+        elif msg_t == "audio":
+            k = {**d["message"]}
+
+            if d["message"]["contentProvider"]["type"] == "line":
+                k.update(
+                    {"contentProvider": WebhookMediaContentProviderLINE(type="line")}
+                )
+            else:
+                k.update(
+                    {
+                        "contentProvider": WebhookAudioContentProviderExternal(
+                            **d["message"]["contentProvider"]
+                        )
+                    }
+                )
+
+            kwargs.update({"message": WebhookAudioMessage(**k)})
+
+        elif msg_t == "file":
+            kwargs.update({"message": WebhookFileMessage(**d["message"])})
+        elif msg_t == "location":
+            kwargs.update({"message": WebhookLocationMessage(**d["message"])})
+        elif msg_t == "sticker":
+            kwargs.update({"message": WebhookStickerMessage(**d["message"])})
         else:
             raise NotImplementedError("unknown message event type")
 
